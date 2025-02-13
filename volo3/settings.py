db14b1c0a96a3c5e8b12259c7ea440c9d86cc3c2
Marcus Parker <marcus.parker@limbsandthings.com>
2025-02-13 19:50:08 +0000

    merge of branches and minor changes to base file

--- volo3/settings.py
+++ volo3/settings.py
@@ -118,18 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-GB'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
